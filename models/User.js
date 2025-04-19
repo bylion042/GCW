c2207dcf5923d3b4847e2c4cd8104782eaea9b31
Bby Lion <codelab042@gmail.com>
2025-04-19 10:12:29 +0100

    first commit

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true, trim: true },
+    termsAccepted: { type: Boolean, required: true }, // Stores checkbox value
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('giftcardwave-cache').then((cache) => {
+        return cache.addAll([
+          '/',  // Root URL
+          '/index',  // Assuming you are serving this from EJS routes, not index.html
+          '/css/index.css',  // Path to the CSS file
+          '/js/scripts.js',  // Path to the JS file
+          '/images/icon-192x192.png',  // Path to the 192x192 icon
+          '/images/icon-512x512.png'   // Path to the 512x512 icon
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);  // Fetch from network if not cached
+      })
+    );
+  });
+  
