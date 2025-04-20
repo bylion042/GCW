f51e9a90bbfa5e7480816c1dc07447fb337d7430
Bby Lion <codelab042@gmail.com>
2025-04-20 11:01:16 +0100

    ADDED PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('giftcardwave-cache').then((cache) => {
         return cache.addAll([
-          '/index', // Root URL
-          '/css/index.css',  // Path to the CSS file
-          '/js/scripts.js',  // Path to the JS file
-          '/images/icon-192x192.png',  // Path to the 192x192 icon
-          '/icons/icon-512x512.png'   // Path to the 512x512 icon
+          '/', 
+          '/css/index.css',  
+          '/js/scripts.js', 
+          '/icons/icon-192.png',  
+          '/icons/icon-512.png'   
         ]);
       })
     );
@@ -15,7 +15,7 @@ self.addEventListener('install', (event) => {
   self.addEventListener('fetch', (event) => {
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);  // Fetch from network if not cached
+        return cachedResponse || fetch(event.request); 
       })
     );
   });
