8bb360fef091046aed4b41d98dab3baba2aa0397
Bby Lion <codelab042@gmail.com>
2025-04-20 03:34:46 +0100

    ADDED PWA

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,11 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('giftcardwave-cache').then((cache) => {
         return cache.addAll([
-          '/',  // Root URL
-          '/index',  // Assuming you are serving this from EJS routes, not index.html
+          '/index', // Root URL
           '/css/index.css',  // Path to the CSS file
           '/js/scripts.js',  // Path to the JS file
-          '/images/favicon-192x192.png',  // Path to the 192x192 icon
-          '/images/favicon-512x512.png'   // Path to the 512x512 icon
+          '/images/icon-192x192.png',  // Path to the 192x192 icon
+          '/icons/icon-512x512.png'   // Path to the 512x512 icon
         ]);
       })
     );
