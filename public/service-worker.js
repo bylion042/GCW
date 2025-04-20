092b1f9d87199805bb4ed4e9fd06ff3917ee9fb1
Bby Lion <codelab042@gmail.com>
2025-04-20 22:16:06 +0100

    ADDED PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,60 +1,14 @@
-// self.addEventListener('install', (event) => {
-//     event.waitUntil(
-//       caches.open('giftcardwave-cache-v2').then((cache) =>{
-//         return cache.addAll([
-//           '/', 
-//           '/css/index.css',  
-//           '/js/scripts.js', 
-//           '/icons/icon-192.png',  
-//           '/icons/icon-512.png'   
-//         ]);
-//       })
-//     );
-//   });
-  
-//   self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//       caches.match(event.request).then((cachedResponse) => {
-//         return cachedResponse || fetch(event.request); 
-//       })
-//     );
-//   });
-  
-
-const CACHE_NAME = 'giftcardwave-cache-v2';
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/css/index.css',
-        '/js/scripts.js',
-        '/icons/icon-192.png',
-        '/icons/icon-512.png',
-        '/manifest.json'
-      ]);
-    })
-  );
+  console.log('Service Worker installed');
+  self.skipWaiting(); // Activate immediately
 });
 
-// Clean up old caches
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      );
-    })
-  );
+  console.log('Service Worker activated');
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+  // Bypass cache completely, just fetch from the network
+  event.respondWith(fetch(event.request));
 });
